a19ec37637bb54a31b5308e651621266dd82e87f
Carlos Britos <clscrea@gmail.com>
2020-10-10 15:58:16 +0200

    first commit

---
+++ src/app/pages/heroes/heroes.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: HeroeModel[] = [];
+  cargando = false;
+
+  constructor(private HeroesService: HeroesService) { }
+
+  ngOnInit(): void {
+
+    this.cargando = true;
+
+    this.HeroesService.getHeroes()
+                      .subscribe( resp => {
+                          this.heroes = resp;
+                          this.cargando = false;
+                      });
+  
+  }
+
+  borrarHeroe( heroe: HeroeModel, i: number ) {
+        
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: `Está seguro que desea borrar a ${ heroe.nombre }`,
+        icon: 'question',
+        showConfirmButton: true,
+        showCancelButton: true 
+    }).then( resp => {
+        if(resp.value){
+          this.heroes.splice(i, 1);
+          this.HeroesService.borrarHeroe(heroe.id).subscribe();
+        }
+    });
+
+    
+  
+  }
+
+}
